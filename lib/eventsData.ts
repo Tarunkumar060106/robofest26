39e9cd79294b9bae083dafe7a2b9dd7e1e91c450
balatharunr <r.balatharun@gmail.com>
2026-04-19 01:57:08 +0530

    add robo soccer img

--- lib/eventsData.ts
+++ lib/eventsData.ts
@@ -49,6 +49,7 @@ export const DEFAULT_EVENTS: EventItem[] = [
     venue: "Vendhir Square, TP2, Hangar",
     team: "4 members",
     color: "#c4410c",
+    backgroundImage: "/images/events/soccer.png",
   },
   {
     number: "04",
